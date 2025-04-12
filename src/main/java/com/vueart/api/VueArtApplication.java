601b31f735c7688e8cee113d7df7ecca292606b9
yjb <yeognjun1324@gmail.com>
2025-04-13 00:28:03 +0900

    init : create project

---
+++ src/main/java/com/vueart/api/VueArtApplication.java
@@ -0,0 +1,13 @@
+package com.vueart.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueArtApplication.class, args);
+	}
+
+}
