8796d726b8c09696a65c206c6b6b5882ca548b2c
byj <yeongjun1324@gmail.com>
2025-04-19 15:38:42 +0900

    feat : create user logic

---
+++ src/main/java/com/vueart/api/entity/User.java
@@ -0,0 +1,50 @@
+package com.vueart.api.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})  // JPA에서 lazy관련 에러 날 경우 사용
+@Entity  // 객체와 테이블 매핑
+@Table(name = "users")
+public class User extends BaseEntity{
+    @Id  // Primary Key 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // AUTO_INCREMENT 설정 (id값이 null일 경우 자동 생성)
+    @Column(name = "user_id")  // 컬럼 지정
+    private Long id;
+
+    @NotNull
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+    @NotNull
+    @Column(name = "email")
+    private String email;
+
+    @Builder
+    public User(Long id, String password, String userName, String email){
+        this.id = id;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
+    }
+
+    public User updatePassword(Long id, String password){
+        return User.builder()
+                .id(this.id)
+                .email(this.email)
+                .userName(this.userName)
+                .password(password)
+                .build();
+    }
+}
---
+++ src/main/java/com/vueart/api/service/user/UserServiceImpl.java
@@ -0,0 +1,55 @@
+package com.vueart.api.service.user;
+
+import com.vueart.api.common.util.AES256Util;
+import com.vueart.api.core.enums.Code;
+import com.vueart.api.core.exception.VueArtApiException;
+import com.vueart.api.dto.request.user.UpdatePasswordRequest;
+import com.vueart.api.entity.User;
+import com.vueart.api.repository.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final AES256Util aes256Util;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    @Transactional
+    public void updatePassword(UpdatePasswordRequest request) {
+        User user = userRepository.findById(request.userId())
+                .orElseThrow(() -> new VueArtApiException(Code.ErrorCode.NOT_FOUND_USER_ID));
+
+        String currentPassword = aes256Util.decode(request.currentPassword());
+        String newPassword = aes256Util.decode(request.newPassword());
+        String confirmPassword = aes256Util.decode(request.confirmPassword());
+
+        log.info("getId: {}", user.getId());
+        log.info("user.getPassword() => {}", aes256Util.decode(user.getPassword()));
+        log.info("Current password is => {}", currentPassword);
+        log.info("New password is => {}", newPassword);
+        log.info("Confirm password is => {}", confirmPassword);
+
+        if (!passwordEncoder.matches(currentPassword, user.getPassword())) {
+            throw new VueArtApiException(Code.ErrorCode.CURRENT_PASSWORD_INCORRECT);
+        }
+
+        if (passwordEncoder.matches(newPassword, user.getPassword())) {
+            throw new VueArtApiException(Code.ErrorCode.NEW_PASSWORD_SAME_AS_OLD);
+        }
+
+        if (!newPassword.equals(confirmPassword)) {
+            throw new VueArtApiException(Code.ErrorCode.PASSWORD_CONFIRMATION_MISMATCH);
+        }
+
+        String encodedNewPassword = passwordEncoder.encode(newPassword);
+        User updated = user.updatePassword(request.userId(), encodedNewPassword);
+        userRepository.save(updated);
+    }
+}
