67046b1a3f391438eef54bbdadf94d28abd5c4f1
byj <yeongjun1324@gmail.com>
2025-04-19 15:37:39 +0900

    feat : use JPA

--- src/main/java/com/vueart/api/VueArtApplication.java
+++ src/main/java/com/vueart/api/VueArtApplication.java
@@ -2,8 +2,10 @@ package com.vueart.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VueArtApplication {
 
 	public static void main(String[] args) {
