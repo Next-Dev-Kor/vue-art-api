c9a2e583a341f7a373de735f992a88b1b5de8e80
yjbaek <yjb1324@tethermax.io>
2025-06-04 13:06:51 +0900

    chore : code line up & delete to annotation

--- src/main/java/com/vueart/api/entity/Notification.java
+++ src/main/java/com/vueart/api/entity/Notification.java
@@ -1,6 +1,12 @@
 package com.vueart.api.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,7 +36,8 @@ public class Notification {
     }
 
     // 기본 생성자 (JPA 필수)
-    protected Notification() {}
+    protected Notification() {
+    }
 
 //    // 생성자 or 빌더 패턴으로 객체 생성
 //    public Notification(User user, String message) {
